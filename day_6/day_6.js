3f2409f9552bc162e85f0455ea8e92b7ef2aee66
Johannes Deivard <jdeivard@outlook.com>
2019-12-06 15:54:02 +0100

    veri badn't

--- day_6/day_6.js
+++ day_6/day_6.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 class Planet {
   constructor(name, orbits) {
     this.name = name
-    // this.orbits = orbits
+    this.orbits = orbits // Planet
     this.orbittingPlanets = new Array()
   }
 }
@@ -18,23 +18,20 @@ function findPlanetPairs(planetPairs, planetToOrbit, orbitterName){
   return matches
 }
 
-
-// Recursive function that traverse all planets to find orbittedPlanet and then add orbitterName to that planet's orbittingPlanets array
-// Always start from COM to traverse all the planets
-function addOrbitter(currentPlanet, planetToOrbit, orbitterName){
-  // console.log("addOrbitter called with parameters: " + currentPlanet + ", " + planetToOrbit + ", " + orbitterName)
-  if (currentPlanet.name == planetToOrbit){
-    // Add the planet to the orbitting array
-    currentPlanet.orbittingPlanets.push(new Planet(orbitterName))
-    console.log("Added orbitter: " + orbitterName + " to planet: " + planetToOrbit)
-    // Return true to break out of the for loop below
-    return true
-  }
-  else {
-    for (let p of currentPlanet.orbittingPlanets){
-      if (addOrbitter(p, planetToOrbit, orbitterName) == true) break
-    }
-  }
+// Create the planet with planet name, a planet is created when all orbitting plannets have been added to the planet's orbittingPlanets array,
+// so this function is recursive.
+// NOTE: This function is based on the planetPairs array, which is based on the input from input.txt
+function createPlanet(planetName, orbits) {
+  // To find the planet orbitting planetName we do a search with the findPlanetPairs function.
+  // If we leave the orbitterName parameter empty (empty string) we will get all pairs that have planetName as the planetToOrbit
+  pairs = findPlanetPairs(planetPairs, planetName, "")
+  var planet =  new Planet(planetName)
+  planet.orbits = orbits
+  pairs.forEach(pair => {
+    // Add all the orbitting planet, after the planets orbitting that planet have been added, after the planets orbitting that planet have been added, after the ... 
+    planet.orbittingPlanets.push(createPlanet(pair.orbitter, planet))
+  })
+  return planet
 }
 
 
@@ -55,9 +52,60 @@ function countDirectAndIndirectOrbits(startPlanet, stepsTaken){
   return sum
 }
 
-var inputData 
+function findYOU(planet) {
+  // console.log(planet)
+  if (planet.name == "YOU")
+    return planet
+  for (let p of planet.orbittingPlanets){
+    var result = findYOU(p)
+    if (result != undefined)
+      return result
+  }
+}
+
+// Start at the planet YOU orbits, with prevPlanet = YOU, 
+function stepToSAN(planet, path, numTransfers, pathToSAN){
+  // console.log(path)
+  // We moved to new planet, so add it to the path
+  path.push(planet.name)
+  // and add 1 to the steps counter
+  numTransfers += 1
+
+  var validMoves = new Array()
+  // Find all possible moves from current planet (moving back to the previous planet is not a valid move)
+  for (var orbitter of planet.orbittingPlanets) {
+    // First we check if we are at the planet that SAN orbits
+    if (orbitter.name == "SAN")
+      // 
+      return {transfers: numTransfers, path: path}
+  
+    // All orbitting planets around this planet is a valid move (except the planets you have visited)
+    if (path.includes(orbitter.name) == false)
+      validMoves.push(orbitter)
+  }
+  // The planet that this planet orbits is also a valid move (unless it is undefined, which it will be when the planet is COM)
+  // or if we have visited it already
+  if (planet.orbits != undefined && path.includes(planet.orbits.name) == false)
+    validMoves.push(planet.orbits)
+  
+  // console.log(validMoves)
+  // Explore all valid moves 
+  for (let targetPlanet of validMoves){
+    var result = stepToSAN(targetPlanet, path, numTransfers)
+    // console.log(result)
+    if (result)
+      return result
+  }
+}
+
+
+
+
+
+
+
 var planetPairs = new Array()
-inputData = fs.readFileSync('input.txt', 'utf8').split("\n")
+var inputData = fs.readFileSync('input.txt', 'utf8').split("\n")
 // inputData = fs.readFileSync('testData.txt', 'utf8').split(/\r?\n\r?/)
 
 // Create the planetPairs array that is filled with planet-orbitter objects
@@ -67,38 +115,16 @@ inputData.forEach(element => {
   planetPairs.push({planet: planet, orbitter: orbitter})
 });
 
-// To find the planet orbitting "COM" we do a search with the findPlanetPairs function.
-// If we leave the orbitterName parameter empty (empty string) we will get all pairs that have "COM" as the planetToOrbit
-// var pairs = findPlanetPairs(planetPairs, "COM", "")
-// console.log(findPlanetPairs(planetPairs, "COM", ""))
-// var COM = new Planet("COM")
-// pairs.forEach( pair => {
-//   COM.orbittingPlanets.push(new Planet(pair.orbitter))
-// })
-// console.log(COM)
+var COM = createPlanet("COM", null)
+// printOrbitterMap(COM)
+// console.log("Direct and indirect orbits: " + countDirectAndIndirectOrbits(COM, 0))
 
-// Create the planet with planet name, a planet is created when all orbitting plannets have been added to the planet's orbittingPlanets array,
-// so this function is recursive.
-// NOTE: This function is based on the planetPairs array, which is based on the input from input.txt
-function createPlanet(planetName) {
-  // To find the planet orbitting planetName we do a search with the findPlanetPairs function.
-  // If we leave the orbitterName parameter empty (empty string) we will get all pairs that have planetName as the planetToOrbit
-  pairs = findPlanetPairs(planetPairs, planetName, "")
-  var planet =  new Planet(planetName)
-  pairs.forEach(pair => {
-    // Add all the orbitting planet, after the planets orbitting that planet have been added, after the planets orbitting that planet have been added, after the ... 
-    planet.orbittingPlanets.push(createPlanet(pair.orbitter))
-  })
-  return planet
-}
+var YOU = findYOU(COM)
+// Start at -1 steps since the recursive function counts the steps from YOU to the planet that SAN is orbitting
+console.log(stepToSAN(YOU.orbits, ["YOU"], -1))
 
-var com = createPlanet("COM")
-// printOrbitterMap(com)
-console.log("Direct and indirect orbits: " + countDirectAndIndirectOrbits(com, 0))
 // console.log(addPlanet("COM"))
 
-// console.log(COM)
-// printOrbitterMap(COM)
 
 
 
